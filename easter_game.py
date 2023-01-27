973a3b5cb825898af74b2c808f5e0abd43a9ea82
Charlies-Kvit <theivangao@gmail.com>
2023-01-27 22:31:26 +0300

    Доделал пасхалку)

--- easter_game.py
+++ easter_game.py
@@ -6,7 +6,12 @@ import os
 pygame.init()
 size = WIDTH, HEIGHT = 800, 800
 DOWN, UP, LEFT, RIGHT = False, False, False, False
+pygame.mixer.music.load(os.path.join("interface", "easter", "sounds", "scare_music.mp3"))
+pygame.mixer.music.play()
+children_laughter = pygame.mixer.Sound(os.path.join("interface/easter/sounds/laugh.mp3"))
 screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+crying_children = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -25,6 +30,17 @@ def load_image(name, colorkey=None):
     return image
 
 
+class CryingBaby(pygame.sprite.Sprite):
+    image = load_image(os.path.join("easter", "easter_cry_child.png"))
+
+    def __init__(self, cords, *group):
+        super().__init__(*group)
+        self.image = CryingBaby.image
+        self.rect = self.image.get_rect()
+        self.rect.x = cords[0]
+        self.rect.y = cords[1]
+
+
 class Hero(pygame.sprite.Sprite):
     image = load_image(os.path.join('easter', "easter_hero.png"))
 
@@ -37,7 +53,12 @@ class Hero(pygame.sprite.Sprite):
         self.U = 1
 
     def update(self, level=None):
-        global UP, DOWN, RIGHT, LEFT
+        global UP, DOWN, RIGHT, LEFT, children_laughter
+        for baby in crying_children.sprites():
+            if pygame.sprite.collide_mask(self, baby):
+                children_laughter.play()
+                crying_children.remove(baby)
+                all_sprites.remove(baby)
         if level is not None:
             ux, uy = 0, 0
             x, y = self.rect.x, self.rect.y
@@ -49,9 +70,17 @@ class Hero(pygame.sprite.Sprite):
                 ux -= self.U
             if RIGHT and self.rect.x < WIDTH - self.image.get_width():
                 ux += self.U
-            # print(level.is_free((x + ux, y + uy)))
             if level.is_free((x + ux, y + uy)):
                 self.rect = self.rect.move(ux, uy)
+        if 385 <= self.rect.x <= 400 and 680 <= self.rect.y <= 695:
+            pygame.mixer.music.stop()
+            if crying_children.sprites():
+                flag = False
+            else:
+                flag = True
+            pygame.quit()
+            scary_function(flag)
+            sys.exit()
 
 
 class Map:
@@ -65,12 +94,12 @@ class Map:
         self.tile_size = self.map.tilewidth
 
     def render(self, screen):
-        for i in range(2):
+        for i in range(4):
             for y in range(self.height):
                 for x in range(self.width):
                     image = self.map.get_tile_image(x, y, i)
                     if image is not None:
-                        if i == 1 and self.flag:
+                        if 1 <= i <= 2 and self.flag:
                             self.map_matrix[x][y] = 1
                         screen.blit(image, (x * self.tile_size, y * self.tile_size))
         self.flag = False
@@ -83,18 +112,31 @@ class Map:
         return flag
 
 
+class Video(object):
+    def __init__(self, path):
+        self.path = path
+
+    def play(self):
+        os.startfile(self.path)
+
+
+class Movie_MP4(Video):
+    type = "MP4"
+
+
 def start_easter_egg():
     global DOWN, UP, RIGHT, LEFT
-    screen = pygame.display.set_mode(size)
-    all_sprites = pygame.sprite.Group()
     clock = pygame.time.Clock()
+    cords = [(607, 15), (774, 127), (369, 707), (16, 369), (753, 752)]
+    for position in cords:
+        CryingBaby(position, all_sprites, crying_children)
     Hero(all_sprites)
     maper = Map()
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            # if event.type == pygame.QUIT:
+            #    running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w or event.key == pygame.K_UP:
                     UP = True
@@ -124,4 +166,14 @@ def start_easter_egg():
     pygame.quit()
 
 
+def scary_function(flag):
+    if flag:
+        path = os.path.join("interface", "easter", "MICHAEL.mp4")
+    else:
+        path = os.path.join("interface", "easter", "Are_you_sure_it's_not_your_fault.mp4")
+    movie = Movie_MP4(path)
+    movie.play()
+
+
 start_easter_egg()
+# scary_function()
